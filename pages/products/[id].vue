dd325c9dbfa455ac20f2d64bb3e4fa8cea410582
Travontaz Lowry <travontazlowry@gmail.com>
2023-06-25 20:22:20 -0400

    added products directory and id.vue

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique placeat
+      corporis veniam, odit dicta maxime totam praesentium esse possimus minima
+      sequi consequuntur asperiores, ea facilis harum quis neque at eaque!
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
